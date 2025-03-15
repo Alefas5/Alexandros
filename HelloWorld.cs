868686def2b52b7b1ac6244f44f5e06d3c4634c8
Alexandros Katsianas <119041902+Alefas5@users.noreply.github.com>
2025-03-15 13:20:27 +0200

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("lqsKKKKASDSADDASDASkjhbqfalksjhgdbsilkj       basikl;jhbS;KDJGHBSd;                           kjhbgs;DKJHBGSLQDik;jfhbweri");
+        print("Hello Alexandros");
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Unity Gaming");
+    }
+}
